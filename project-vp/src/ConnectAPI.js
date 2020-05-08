da72f37c2adc4e3ae06cc789f6f0901eef2576d5
villa999 <vietanh.hn.4078@gmail.com>
2020-05-08 23:04:52 +0700

    add loadmore for axios

--- project-vp/src/ConnectAPI.js
+++ project-vp/src/ConnectAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default function loadData ()  {
      return axios({
           method: 'get',
-          url: 'https://jsonplaceholder.typicode.com/posts/',
+          url: 'https://jsonplaceholder.typicode.com/posts',
           data: null    
      }).catch((err)=>{
           console.log(err)
--- project-vp/src/components/Main.js
+++ project-vp/src/components/Main.js
@@ -6,55 +6,86 @@ class Main extends React.Component {
      constructor(props) {
           super(props)
           this.state = {
-               items:[],
+               itemsAPI: [],
                visible: 9,
-               error:false
+               loadingState: false
           }
-          this.loadMore = this.loadMore.bind(this)
      }
 
      componentDidMount() {
-          loadData(this.state.visible).then((res) => {
-                    this.props.run(res.data)
-                    this.setState({items:this.props.data})
+
+          loadData().then((res) => {
+               this.props.run(res.data)
           })
      }
 
-     loadMore(){
-          this.setState((prev)=>{
-               return {visible: prev.visible + 9}
+     onClickLoadMore = (e) => {
+          e.preventDefault()
+          this.loadMore()
+
+     }
+     mapData = () => {
+          var items = this.props.data.map((val,ind)=>{
+               if(val.title.length > 0){
+               return val.title
+               }
           })
+          return items
      }
 
-
-
-     render() {
-          var items = this.state.items.slice(0,this.state.visible).map((val,ind)=>{
-               return <div className="col-lg-4 col-md-6" key={ind}>
-               <div className="card h-100">
-                    <div className="single-post post-style-1">
-                         <div className="blog-image"><img src="images/marion-michele-330691.jpg" alt="Blog Image" /></div>
-                         <a className="avatar" href="123"><img src="images/icons8-team-355979.jpg" alt="Profile Image" /></a>
-                         <div className="blog-info">
-                              <h4 className="title"><a href="123"><b>{val.title}</b></a></h4>
-                              <ul className="post-footer">
-                                   <li><a href="123"><i className="ion-heart" />57</a></li>
-                                   <li><a href="123"><i className="ion-chatbubble" />6</a></li>
-                                   <li><a href="123"><i className="ion-eye" />138</a></li>
-                              </ul>
+     
+     displayItems = () => {
+          var dataTitle = this.mapData()
+          var items = []
+          for (var i = 0; i < this.state.visible; i++) {
+               if(dataTitle[i] !== ""){
+               items.push(<div className="col-lg-4 col-md-6" key={i} >
+                    <div className="card h-100">
+                         <div className="single-post post-style-1">
+                              <div className="blog-image"><img src="images/marion-michele-330691.jpg" alt="Blog Image" /></div>
+                              <a className="avatar" href="123"><img src="images/icons8-team-355979.jpg" alt="Profile Image" /></a>
+                              <div className="blog-info">
+                                   <h4 className="title"><a href="123"><b>{dataTitle[i]}</b></a></h4>
+                                   <ul className="post-footer">
+                                        <li><a href="123"><i className="ion-heart" />57</a></li>
+                                        <li><a href="123"><i className="ion-chatbubble" />6</a></li>
+                                        <li><a href="123"><i className="ion-eye" />138</a></li>
+                                   </ul>
+                              </div>
                          </div>
                     </div>
-               </div>
-          </div>
-        
-          })
+               </div>)}
+          }
+          return items
+     }
+
+
+     loadMore = () => {
+          var x 
+          if(this.props.data.length - this.state.visible >=9 ){
+               x = 9
+          }else {
+               x = (this.props.data.length - this.state.visible)
+          }
+          if (this.state.loadingState) {
+               return;
+          } else {
+               this.setState({ loadingState: true });
+               setTimeout(() => {
+                    this.setState({ visible: this.state.visible + x, loadingState: false })                    
+               }, 1000)
+
+          }
+     }
+
+     render() {
           return (
                <section className="blog-area section">
                     <div className="container">
                          <div className="row">
-                              {items}
+                              {this.displayItems()}
                          </div>
-                         {this.state.visible < this.props.data.length && <a className="load-more-btn" href="123" onClick={this.loadMore}><b>LOAD MORE</b></a>}
+                         {(this.state.visible < this.props.data.length)?<a className="load-more-btn" href="123" onClick={(e) => this.onClickLoadMore(e)}><b>LOAD MORE</b></a>:false}
                     </div>
                </section>
           )
